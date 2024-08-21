7c354c11a82d17ac609febcabe005abd2277653a
Muhammed Afreen K N <96134286+aabi8113@users.noreply.github.com>
2024-08-21 11:13:59 +0530

    Update Item.js

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -2,13 +2,9 @@ import React from "react";
 import "./Item.css";
 
 function Item({ name, rating, price, saleDiscount, image, brand }) {
-  // Construct the full image path
-  const imageUrl = `${process.env.PUBLIC_URL}${image}`;
-  console.log(imageUrl);
-
   return (
     <div className="item-card">
-      <img src={imageUrl} alt={name} width="100%" />
+      <img src={image} alt={name} width="100%" />
       <div className="item-brand">{brand}</div>
       <div className="item-name">{name}</div>
       <div className="item-info">
